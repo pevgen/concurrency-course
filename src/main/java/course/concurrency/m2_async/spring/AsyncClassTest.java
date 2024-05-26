b9ad67a88821f11cb8ce321787cd88744ce79845
Evgeny Polyak <epolyak@luxoft.com>
2024-05-26 12:09:24 +0300

    Эксперименты с @Async (разница при вызове внутри того же класса и вызове из другого класса)

--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -3,6 +3,7 @@ package course.concurrency.m2_async.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +17,13 @@ public class AsyncClassTest {
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
+    @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
+       // internalTask();
     }
 
+    @Async
     public void internalTask() {
         System.out.println("internalTask: " + Thread.currentThread().getName());
     }
--- src/main/java/course/concurrency/m2_async/spring/SpringBootAsyncTest.java
+++ src/main/java/course/concurrency/m2_async/spring/SpringBootAsyncTest.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringBootAsyncTest {
 
     @Autowired
@@ -16,6 +18,7 @@ public class SpringBootAsyncTest {
     @EventListener(ApplicationReadyEvent.class)
     public void actionAfterStartup() {
         testClass.runAsyncTask();
+        testClass.internalTask();
     }
 
     public static void main(String[] args) {
